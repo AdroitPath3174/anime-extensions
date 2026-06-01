b16c6648d721648f5b059e64b50715e2b9c7b3e9
AdroitPath3174 <1234minecraft@atomicmail.io>
2026-06-01 22:51:09 +0530

    Update Toonstream.kt

--- src/en/toonstream/src/eu/kanade/tachiyomi/animeextension/en/toonstream/Toonstream.kt
+++ src/en/toonstream/src/eu/kanade/tachiyomi/animeextension/en/toonstream/Toonstream.kt
@@ -1,11 +1,11 @@
 package eu.kanade.tachiyomi.animeextension.en.toonstream
 
-import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
 import eu.kanade.tachiyomi.animesource.model.SAnime
 import eu.kanade.tachiyomi.animesource.model.SEpisode
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.ParsedAnimeHttpSource
+import eu.kanade.tachiyomi.network.GET
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
@@ -16,10 +16,9 @@ class Toonstream : ParsedAnimeHttpSource() {
     override val lang = "en"
     override val supportsLatest = true
 
-    // 1. POPULAR ANIME
     override fun popularAnimeSelector() = "article.item"
     override fun popularAnimeNextPageSelector() = "div.pagination a.next"
-    
+
     override fun popularAnimeFromElement(element: Element): SAnime {
         val anime = SAnime.create()
         anime.setUrlWithoutDomain(element.select("a").attr("href"))
@@ -28,19 +27,16 @@ class Toonstream : ParsedAnimeHttpSource() {
         return anime
     }
 
-    // 2. SEARCH
     override fun searchAnimeSelector() = popularAnimeSelector()
     override fun searchAnimeNextPageSelector() = popularAnimeNextPageSelector()
     override fun searchAnimeFromElement(element: Element) = popularAnimeFromElement(element)
     override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList) = GET("$baseUrl/page/$page/?s=$query", headers)
 
-    // 3. LATEST EPISODES
     override fun latestUpdatesSelector() = popularAnimeSelector()
     override fun latestUpdatesNextPageSelector() = popularAnimeNextPageSelector()
     override fun latestUpdatesFromElement(element: Element) = popularAnimeFromElement(element)
     override fun latestUpdatesRequest(page: Int) = GET("$baseUrl/episodes/page/$page/", headers)
 
-    // 4. ANIME DETAILS
     override fun animeDetailsParse(document: Document): SAnime {
         val anime = SAnime.create()
         anime.title = document.select("h1").text()
@@ -48,7 +44,6 @@ class Toonstream : ParsedAnimeHttpSource() {
         return anime
     }
 
-    // 5. EPISODES
     override fun episodeListSelector() = "ul.episodios li"
     override fun episodeFromElement(element: Element): SEpisode {
         val episode = SEpisode.create()
@@ -57,7 +52,6 @@ class Toonstream : ParsedAnimeHttpSource() {
         return episode
     }
 
-    // 6. VIDEO PLAYER (Placeholder)
     override fun videoListParse(response: okhttp3.Response): List<Video> {
         return listOf(Video("https://example.com/video.mp4", "Toonstream Video", "https://example.com/video.mp4"))
     }
